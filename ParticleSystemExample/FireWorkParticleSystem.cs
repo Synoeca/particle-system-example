a7818f5ec193d3070e9af41a71269616e29afd80
Synoeca <synoeca523@ksu.edu>
2024-09-26 20:19:53 -0500

    Complete Particle System Exercise

---
+++ ParticleSystemExample/FireWorkParticleSystem.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSystemExample
+{
+	public class FireWorkParticleSystem : ParticleSystem
+	{
+		private Color[] _colors = new Color[]
+		{
+			Color.Fuchsia,
+			Color.Red,
+			Color.Crimson,
+			Color.CadetBlue,
+			Color.Aqua,
+			Color.HotPink,
+			Color.LimeGreen
+		};
+
+		private Color color;
+
+		public FireWorkParticleSystem(Game game, int maxExplosions) : base(game, maxExplosions * 25) { }
+
+		protected override void InitializeConstants()
+		{
+			textureFilename = "circle";
+
+			minNumParticles = 20;
+			maxNumParticles = 25;
+
+			blendState = BlendState.Additive;
+			DrawOrder = AdditiveBlendDrawOrder;
+		}
+
+		protected override void InitializeParticle(ref Particle p, Vector2 where)
+		{
+			var velocity = RandomHelper.NextDirection() * RandomHelper.NextFloat(40, 200);
+
+			var lifetime = RandomHelper.NextFloat(0.5f, 1.0f);
+
+			var acceleration = -velocity / lifetime;
+
+			var rotation = RandomHelper.NextFloat(-MathHelper.PiOver4, MathHelper.PiOver4);
+
+			var angularVelocity = RandomHelper.NextFloat(-MathHelper.PiOver4, MathHelper.PiOver4);
+
+			var scale = RandomHelper.NextFloat(14, 26);
+
+			p.Initialize(where, velocity, acceleration, color, lifetime: lifetime, rotation: rotation, angularVelocity: angularVelocity, scale: scale);
+		}
+
+		protected override void UpdateParticle(ref Particle particle, float dt)
+		{
+			base.UpdateParticle(ref particle, dt);
+
+			float normalizedLifetime = particle.TimeSinceStart / particle.Lifetime;
+
+			particle.Scale = 0.1f + 0.25f * normalizedLifetime;
+		}
+
+		public void PlaceFireWork(Vector2 where)
+		{
+			color = _colors[RandomHelper.Next(_colors.Length)];
+			AddParticles(where);
+		}
+	}
+}
--- ParticleSystemExample/ParticleSystemExampleGame.cs
+++ ParticleSystemExample/ParticleSystemExampleGame.cs
@@ -7,15 +7,23 @@ namespace ParticleSystemExample
     /// <summary>
     /// An example game demonstrating the use of particle systems
     /// </summary>
-    public class ParticleSystemExampleGame : Game
+    public class ParticleSystemExampleGame : Game, IParticleEmitter
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        /// <summary>
-        /// Constructs an instance of the game
-        /// </summary>
-        public ParticleSystemExampleGame()
+        private MouseState _priorMouse;
+        private ExplosionParticleSystem _explosions;
+        private FireWorkParticleSystem _fireworks;
+
+		public Vector2 Position { get; set; }
+
+		public Vector2 Velocity { get; set; }
+
+		/// <summary>
+		/// Constructs an instance of the game
+		/// </summary>
+		public ParticleSystemExampleGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
@@ -28,6 +36,17 @@ namespace ParticleSystemExample
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            RainParticleSystem rain = new RainParticleSystem(this, new Rectangle(100, -20, 500, 10));
+            Components.Add(rain);
+
+            _explosions = new ExplosionParticleSystem(this, 20);
+            Components.Add(_explosions);
+
+            _fireworks = new FireWorkParticleSystem(this, 20);
+            Components.Add(_fireworks);
+
+            PixieParticleSystem pixie = new(this, this);
+            Components.Add(pixie);
 
             base.Initialize();
         }
@@ -52,8 +71,23 @@ namespace ParticleSystemExample
                 Exit();
 
             // TODO: Add your update logic here
-            
-            base.Update(gameTime);
+            MouseState currentMouse = Mouse.GetState();
+            Vector2 mousePosition = new Vector2(currentMouse.X, currentMouse.Y);
+
+            if (currentMouse.LeftButton == ButtonState.Pressed && _priorMouse.LeftButton == ButtonState.Released)
+            {
+                _explosions.PlaceExplosion(mousePosition);
+            }
+
+			if (currentMouse.RightButton == ButtonState.Pressed && _priorMouse.RightButton == ButtonState.Released)
+			{
+				_fireworks.PlaceFireWork(mousePosition);
+			}
+
+            Velocity = mousePosition - Position;
+            Position = mousePosition;
+
+			base.Update(gameTime);
         }
 
         /// <summary>
